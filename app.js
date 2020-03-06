340462c4306f1eec57c98a3d861aab21ec5c116f
wooseung <shdntmd44@naver.com>
2020-03-06 21:30:23 +0900

    많이 추가함

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ import { localsMiddleware } from "./middlewares";
 const app = express();
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 
 app.use(cookieParser());
 app.use(bodyParser.json());
